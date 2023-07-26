ba4d18d7c3c04b7a4b03a7f9b1f0b4c61697fc48
flyduck-dev <flyduck0dev@gmail.com>
2023-07-27 07:16:37 +0900

    [Add]이진탐색

---
+++ 이진탐색.py
@@ -0,0 +1,14 @@
+def sol(nums, target):
+    start = 0
+    end = len(nums)-1
+    while(start <= end):
+        mid = start + end // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            start += 1
+        else:
+            end -= 1
+    
+print(sol([2,5,7,8,10,15,20,24,25,30], 8))
+print(sol([-3,0,2,5,8,9,12,15], 0))
